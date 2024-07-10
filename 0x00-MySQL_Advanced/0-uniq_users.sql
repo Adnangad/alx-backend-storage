42314940ef679da5755004468dfe1e42ee5e01c9
Adnangad <gardobuyaadnan@gmail.com>
2024-07-10 10:55:55 +0000

    0-created a table users with constraints

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- creates a table users
+CREATE DATABASE IF NOT EXISTS holberton;
+CREATE TABLE IF NOT EXISTS holberton.users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255));
