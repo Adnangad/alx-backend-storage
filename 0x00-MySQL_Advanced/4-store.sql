4d0b3eb880ddc5c55131e83084762c83d77af350
Adnangad <gardobuyaadnan@gmail.com>
2024-07-10 14:47:06 +0000

    3 and 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,5 @@
+-- creates a before trigger
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+	UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
---
+++ 0x00-MySQL_Advanced/temp.sql
@@ -0,0 +1,1 @@
+drop trigger reduce_quantity;
