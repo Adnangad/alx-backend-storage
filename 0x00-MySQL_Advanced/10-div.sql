f78b4298e5c102c2175dbb86803c189ac06b2787
Adnangad <gardobuyaadnan@gmail.com>
2024-07-11 13:22:38 +0000

    10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -9,10 +9,10 @@ BEGIN
     IF b = 0 THEN
         SET result = 0;
     ELSE
-        SET result = CAST(a AS FLOAT) / b;
+        SET result = a/b;
     END IF;
     
     RETURN result;
 END $$
 
-DELIMITER ;
+DELIMITER;
