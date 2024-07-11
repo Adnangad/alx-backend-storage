43284b2c9b3fd42e4c8cf4527faa23a0f22cdcaf
Adnangad <gardobuyaadnan@gmail.com>
2024-07-11 11:04:56 +0000

    10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,7 +4,15 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 BEGIN
-    RETURN IF(b = 0, 0, CAST(a AS FLOAT) / b);
+    DECLARE result FLOAT;
+    
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = CAST(a AS FLOAT) / b;
+    END IF;
+    
+    RETURN result;
 END $$
 
 DELIMITER ;
