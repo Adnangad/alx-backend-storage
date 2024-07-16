3111fd13454b53a970bf819d0798e8de018aaee3
Adnangad <gardobuyaadnan@gmail.com>
2024-07-16 04:59:36 -0700

    12-statz

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,20 +5,24 @@ This module retreives info from  a db
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_col = client.logs.nginx
-num = nginx_col.count_documents({})
-print(f"{num} logs")
-print("Methods:")
-get = nginx_col.count_documents({"method": "GET"})
-post = nginx_col.count_documents({"method": "POST"})
-put = nginx_col.count_documents({"method": "PUT"})
-patch = nginx_col.count_documents({"method": "PATCH"})
-delete = nginx_col.count_documents({"method": "DELETE"})
-stat = nginx_col.count_documents({"path": "/status"})
-print(f"\tmethod GET: {get}")
-print(f"\tmethod POST: {post}")
-print(f"\tmethod PUT: {put}")
-print(f"\tmethod PATCH: {patch}")
-print(f"\tmethod DELETE: {delete}")
-print(f"{stat} status check")
+if __name__ == "__main__":
+    """
+    Implements the main code
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_col = client.logs.nginx
+    num = nginx_col.count_documents({})
+    print(f"{num} logs")
+    print("Methods:")
+    get = nginx_col.count_documents({"method": "GET"})
+    post = nginx_col.count_documents({"method": "POST"})
+    put = nginx_col.count_documents({"method": "PUT"})
+    patch = nginx_col.count_documents({"method": "PATCH"})
+    delete = nginx_col.count_documents({"method": "DELETE"})
+    stat = nginx_col.count_documents({"path": "/status"})
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{stat} status check")
