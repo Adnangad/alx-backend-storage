8f9e4fa9635d1e657927e4cdf7242adeca4e8b47
Adnangad <gardobuyaadna@gmail.com>
2024-07-17 14:41:32 +0000

    0-storage

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module manipulates the redis db
+"""
+import redis
+import uuid
+
+
+class Cache:
+    def __init__(self):
+        """
+        Initializes vars
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data):
+        """
+        generates a key and inserts data to redis db
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
