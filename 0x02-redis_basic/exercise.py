db22a85aac9f1625da434780cf6aebc9437234bd
Adnangad <gardobuyaadna@gmail.com>
2024-07-17 15:10:44 +0000

    3-added call history  decorator and 4-added replay method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,6 +22,24 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    A decorator that create input and output list keys
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """
+        wrapper function
+        """
+        input_key = f"{method.__qualname__}:inputs"
+        out_key =   f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwds)
+        self._redis.rpush(out_key, str(result))
+        return result
+    return wrapper
+
+
 class Cache:
     def __init__(self):
         """
@@ -31,6 +49,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         generates a key and inserts data to redis db
@@ -60,3 +79,19 @@ class Cache:
         Returns an int
         """
         return self.get(key, fn=int)
+
+
+def replay(method):
+    """
+    display the history of calls of a particular function
+    """
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+
+    inputs = cache._redis.lrange(input_key, 0, -1)
+    outputs = cache._redis.lrange(output_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+
+    for inp, outp in zip(inputs, outputs):
+        print(f"{method.__qualname__}(*{inp.decode('utf-8')}) -> {outp.decode('utf-8')}")
